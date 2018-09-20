b2e7944cca29335e0addfae3060c13e8a9f4d527
Issa Rice <riceissa@gmail.com>
2018-09-19 17:08:02 -0700

    more

--- proc.py
+++ proc.py
@@ -5,16 +5,71 @@ import glob
 import re
 from bs4 import BeautifulSoup
 
-def main():
-    fps = glob.glob("data/*.html")
-    for fp in sorted(fps):
-        date_string = fp[len("data/"):-len(".html")]
-        listing = get_listing(date_string)
-        print(listing)
+NAME = 0
+TITLE = 1
+START_DATE = 2
 
-def get_listing(date_string):
+def main():
+
+    # Keeps track of the current positions state. Each element of state is a
+    # 3-tuple (name, title, start_date). The idea is to add tuples to state
+    # when a new position appears, and to remove from state (and print the SQL
+    # insert line) when a position disappears.
+    state = []
+
+    prev_listing = None
+    for fp in sorted(glob.glob("data/*.html")):
+        date_string = fp[len("data/"):-len(".html")]
+        listing = get_listing(fp)
+        if prev_listing is None:
+            # This is the first snapshot, so add everyone
+            for name, title in listing:
+                state.append((name, title, date_string))
+        else:
+            new_state = []
+            for name, title, start_date in state:
+                if person_in_listing(name, listing):
+                    # This person is still at the org, so keep them
+                    if position_title_same(name, title, listing):
+                        new_state.append((name, title, start_date))
+                    else:
+                        print_sql_line(name, title, start_date, date_string)
+                        new_state.append((name, position_title(name, listing), date_string))
+                else:
+                    # The person left the org, so print the position
+                    # information.
+                    print_sql_line(name, title, start_date, date_string)
+            state = new_state
+            for name, title in listing:
+                if not person_in_state(name, state):
+                    # This person is new to the organization, so record them in
+                    # state.
+                    state.append((name, title, date_string))
+
+                # There is no else-case because the case of a position title
+                # change is already handled in the loop over the state above.
+
+        prev_listing = listing
+
+def position_title(name, listing):
+    for item in listing:
+        if item[NAME] == name:
+            return item[TITLE]
+
+def person_in_list(name, lst):
+    return any(t[NAME] == name for t in lst)
+
+def position_title_same(name, title, lst):
+    for item in lst:
+        if item[NAME] == name:
+            return item[TITLE] == title
+    raise ValueError("Person not found in list")
+
+
+def get_listing(filepath):
+    """Get a single team member listing given the snapshot filepath."""
     team_members = []
-    with open(f"data/{date_string}.html", "r") as f:
+    with open(filepath, "r") as f:
         soup = BeautifulSoup(f, "lxml")
         for biorow in soup.find_all("div", {"class": "bioRow"}):
             if biorow.find("div", {"class": "staff"}):
